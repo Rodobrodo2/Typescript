8c608a90f335c0d40b4167d1d546940ab7b310a3
Rodobrodo2 <rodondolo2@gmail.com>
2025-02-08 14:25:04 +0100

    Installa ts-node e nodemon

---
+++ todo-list/src/index.ts
@@ -0,0 +1,42 @@
+// string - number - boolean
+const myName: string = "Redi";
+const myAge: number = 25;
+const isAuth: boolean = true;
+
+// Oggetti
+const user: { name: string, age: number, isAuth: boolean } = {
+    name: "Alessandra",
+    age: 33,
+    isAuth: true
+};
+
+// Types
+type CustomArray = (string | number | null)[];
+
+// Interface
+interface User {
+    name: string,
+    age: number, 
+    isAuth: boolean
+}
+
+// Array
+const array: CustomArray = ["blue", "red", "green", "yellow", 34, 45, null];
+
+// Funzioni
+const myFunction = (array: (string | number | null)[], obj: { name: string, age: number, isAuth: boolean }): void => {
+    console.log(array, obj);
+};
+
+myFunction(array, user);
+
+// Types - interface - optionals
+const printArray = (arr: CustomArray, obj: User) => {
+    console.log(arr, obj)
+}
+
+printArray(array, {
+    name: "ale",
+    age: 33,
+    isAuth: true,
+});
